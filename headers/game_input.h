adb64c4972b0f2b3f29dec4714b9051bb84e73fa
mirco0 <mircomicromac@gmail.com>
2025-02-02 22:53:20 +0100

    cleaned game input logic

--- headers/game_input.h
+++ headers/game_input.h
@@ -3,9 +3,11 @@
 
 #include <SDL2/SDL_rect.h>
 
+// Gives the indexes of a [N][N] grid given an index of a [2N] array 
 SDL_Point INPUT_index_nmatrix(int ind, int n);
+// Gives the index of a [2N] array given the indexes of a [N][N] grid  
 int INPUT_nmatrix_index(int x, int y, int n);
-SDL_Point INPUT_mouse_pos_to_grid(SDL_Point* mouse_pos,SDL_Rect* bounds);
-void INPUT_rawmouse_to_game(SDL_Point* pointer_pos, SDL_Rect* bounds, SDL_Rect* smallbounds, int* index, int* sub_index);
+// Gives the indexes of the GAME given the pointer position and the two grids bounds  
+int INPUT_rawmouse_to_game(const SDL_Point* pointer_pos, const SDL_Rect* bounds, const SDL_Rect* smallbounds, int n, int* dst_index, int* dst_sub_index);
 
 #endif
--- src/game_input.c
+++ src/game_input.c
@@ -1,61 +1,38 @@
 #include <SDL2/SDL_rect.h>
 #include <game_input.h>
+#include <stdio.h>
 
 SDL_Point INPUT_index_nmatrix(int ind, int n){
   SDL_Point grid_pos;
-  grid_pos.x = ind/n;
-  grid_pos.y = ind%n;
+  grid_pos.x = ind%n;
+  grid_pos.y = ind/n;
   return grid_pos;
 }
 
-int INPUT_nmatrix_index(int x, int y,int n){
-  return x*n+y;
+int INPUT_nmatrix_index(int x, int y, int n){
+  return y*n+x;
 }
 
-int clamp(int* c, int min, int max){
-	if(*c < min){
-		*c = min;
-		return 1;
-	}
-	if(*c > max){
-		*c = max;
-		return 1;
-	}
-	return 0;
-}
-
-
-void GR_mat_to_coord(int i, int j, int n, SDL_Rect* bounds, SDL_Point* dst_coords){
-  dst_coords->x = bounds->x + (float)bounds->w / n * i;
-  dst_coords->y = bounds->y + (float)bounds->h / n * j;
-}
-
-void GR_coord_to_nmat(SDL_Point* coord, int n, SDL_Rect* bounds, int* dst_i, int* dst_j){
+void INPUT_coord_to_nmat(const SDL_Point* coord, int n, const SDL_Rect* bounds, int* dst_x, int* dst_y){
   int x = coord->x - bounds->x;
   int y = coord->y - bounds->y;
-  *dst_i = (float)n / bounds->w*x;
-  *dst_j = (float)n / bounds->h*y;
-	clamp(dst_i,0, n);
-	clamp(dst_j,0, n);
-  //TODO: CLAMP 0,n
+  *dst_x = (float)n / bounds->w*x;
+  *dst_y = (float)n / bounds->h*y;
 }
 
-void INPUT_rawmouse_to_game(SDL_Point* pointer_pos, SDL_Rect* bounds, SDL_Rect* smallbounds, int* index, int* sub_index){
-	int a, b;
-  GR_coord_to_nmat(pointer_pos, 3, bounds, &a, &b);
-	*index = INPUT_nmatrix_index(b, a, 3);
-	
-	smallbounds->x = bounds->x + smallbounds->w*a;
-	smallbounds->y = bounds->y + smallbounds->h*b;
-	
-  GR_coord_to_nmat(pointer_pos, 3, smallbounds, &a, &b);
-	*sub_index = INPUT_nmatrix_index(b, a, 3);
-	
+int INPUT_rawmouse_to_game(const SDL_Point* pointer_pos, const SDL_Rect* bounds, const SDL_Rect* smallbounds, int n, int* dst_index, int* dst_sub_index){
+  if(!SDL_PointInRect(pointer_pos,bounds)){
+    *dst_index = -1;
+    *dst_sub_index = -1; 
+    return 0;
+  }
+  int x, y;
+  INPUT_coord_to_nmat(pointer_pos, n, bounds, &x, &y);
+  *dst_index = INPUT_nmatrix_index(x,y,n);
+	SDL_Rect offset = *smallbounds;
+  offset.x += offset.w * x + bounds->x;
+  offset.y += offset.h * y + bounds->y;
+  INPUT_coord_to_nmat(pointer_pos, n, &offset, &x, &y);
+  *dst_sub_index = INPUT_nmatrix_index(x,y,n);
+  return 1;
 }
-
-// SDL_Point INPUT_mouse_pos_to_grid(SDL_Point* mouse_pos,SDL_Rect* bounds){
-//   int mat_x = 9./bounds->w * mouse_pos->x - (bounds->x);
-//   int mat_y = 9./bounds->h * mouse_pos->y - (bounds->y);
-//   return (SDL_Point){.x = mat_x, .y = mat_y};
-
-// }
