6b2a7cdad46f686f344aba6a599e11ce9dc7f639
mirco0 <mircomicromac@gmail.com>
2025-02-02 22:56:29 +0100

    first working version

--- headers/game_handler.h
+++ headers/game_handler.h
@@ -1,9 +1,12 @@
 #ifndef GAME_HANDLER
 #define GAME_HANDLER
 
-int GAME_play_move(int turn, int grid_index, int sub_index);
-int GAME_get_available_cells(int gi, int smi, int* indexes, int* n_indexes);
 
-void print();
+int GAME_init();
+void get_GAME_STATE(int a[9][10]);
+int GAME_play_move(int index, int sub_index);
+int GAME_get_available_cells(int index, int* indexes, int* n_indexes);
+int GAME_get_turn();
+int GAME_validate_move(int index,int sub_index);
 
 #endif 
--- src/game_handler.c
+++ src/game_handler.c
@@ -1,5 +1,4 @@
 #include <game_handler.h>
-#include <stdio.h>
 
 #define CROSS_TURN 1
 #define CIRCLE_TURN 0
@@ -7,7 +6,7 @@
 int GAME_STATE[9][10] = {0};
 
 int available_subgrids[9] = {0}; 
-int turn;
+int turn = 0;
 
 int GAME_init(){
   //TODO: generalize for nxm
@@ -18,17 +17,55 @@ int GAME_init(){
   }
   return 0;
 }
+void check_update_board(int index){
+  for(int i = 0; i<9; i+=3){
+    if( GAME_STATE[index][i] == GAME_STATE[index][i+1] && GAME_STATE[index][i+1] == GAME_STATE[index][i+2]){
+      GAME_STATE[index][9] = GAME_STATE[index][i];
+      return;
+    }
+  }
+  
+  for(int i = 0; i<3; i++){
+    if( GAME_STATE[index][i] == GAME_STATE[index][i+3] && GAME_STATE[index][i+3] == GAME_STATE[index][i+6]){
+      GAME_STATE[index][9] = GAME_STATE[index][i];
+      return;
+    }
+  }
+  
+  if( GAME_STATE[index][2] == GAME_STATE[index][4] && GAME_STATE[index][4] == GAME_STATE[index][6]){
+    GAME_STATE[index][9] = GAME_STATE[index][2];
+    return;
+  }
 
-int GAME_play_move(int turn, int grid_index, int sub_index){
-  GAME_STATE[grid_index][sub_index] = turn;
-  return 0;
+
+  if( GAME_STATE[index][0] == GAME_STATE[index][4] && GAME_STATE[index][4] == GAME_STATE[index][8]){
+    GAME_STATE[index][9] = GAME_STATE[index][0];
+    return;
+  }
 }
 
-int GAME_get_available_cells(int gi, int smi, int* indexes, int* n_indexes){
-  //TODO: smi not needed
-  //se la cella 'gi' è finita => restituisci gli indici di tutte quelle non finite
+int GAME_play_move(int index, int sub_index){
+  if(GAME_validate_move(index,sub_index)){
+    GAME_STATE[index][sub_index] = turn;
+    turn = !turn;
+    check_update_board(index);
+    return 1;
+  }
+  return 0;
+
+}
+
+void get_GAME_STATE(int a[9][10]){
+  for(int i = 0; i<9; i++){
+    for(int j = 0; j<10; j++){
+      a[i][j] = GAME_STATE[i][j];
+    }
+  }
+}
+
+int GAME_get_available_cells(int index, int* indexes, int* n_indexes){
   int k = 0;
-  if(GAME_STATE[gi][9] != -1){
+  if(GAME_STATE[index][9] != -1){
     for(int i = 0; i<9; i++){
       if(GAME_STATE[i][9] == -1)
         indexes[k++] = i;
@@ -36,17 +73,17 @@ int GAME_get_available_cells(int gi, int smi, int* indexes, int* n_indexes){
     *n_indexes = k;
     return 1;
   }
-  indexes[0] = gi;
+  indexes[0] = index;
   *n_indexes = 1;
   return 0;
 }
 
+int GAME_get_turn(){
+  return turn;
+}
 
-void print(){
-  for(int i = 0; i<9; i++){
-    for(int j = 0; j<9; j++){
-      printf("%d ",GAME_STATE[i][j]);
-    }
-    printf("\n");
-  }
+int GAME_validate_move(int index, int sub_index){
+  if(GAME_STATE[index][sub_index] != -1) return 0;
+  if(GAME_STATE[index][9] != -1) return 0;
+  return 1;
 }
